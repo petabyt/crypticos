72f97409e0cab3819867219c73a6d74e20df640a
Daniel <daniel@thinkpadr52>
2020-10-20 13:48:27 -0400

    Change pgrm_c to clear code

--- x86/build.sh
+++ x86/build.sh
@@ -9,7 +9,7 @@ node ../../casm/cli/node.js ../main.casm >> build/a.o
 echo "; Main program file. Store programs here." >> build/pgrms.asm
 echo "pgrm_a: db '!%****++.*****++++.*++..+++.!********++++.!******++.%*.****++++.+++.!%%*+++.!%%.!******+++.a', 0 ; Hello World" >> build/pgrms.asm
 echo "pgrm_b: db '!%-->|<.>dd!<^>a!%*++^a!++^!?<+>!+^$|', 0 ; Count 1-10" >> build/pgrms.asm
-echo "pgrm_c: db '!%***>!%***+<.>.>', 0 ;" >> build/pgrms.asm
+echo "pgrm_c: db '!**.........................+++.', 0 ;" >> build/pgrms.asm
 echo "pgrm_d: db '`cat build/a.o`', 0" >> build/pgrms.asm
 
 nasm bootloader.asm -f bin -o build/boot.bin
