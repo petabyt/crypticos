6fb0748bfd45313b94fbc77ff88c76db377fea89
Daniel <brikbusters@gmail.com>
2021-01-19 19:16:46 -0500

    misc

--- x86/build.sh
+++ x86/build.sh
@@ -1,5 +1,5 @@
 touch build.asm
-echo "db \"`casm -a ../demo/screen.casm`\", 0" >> build.asm
+echo "db \"`casm -a ../main.casm`\", 0" >> build.asm
 nasm -f bin main.asm
 qemu-system-x86_64 main
 rm -rf build.asm main
